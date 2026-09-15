dbd60495aea595961652854e77c011b26277d740
Alan <122044260@upq.edu.mx>
2026-09-15 13:34:20 -0600

    introduccion arreglos

---
+++ arreglos.py
@@ -0,0 +1,29 @@
+#DECLARANDO UN ARREGLO
+numeros = [10 , 20, 30, 40, 50]
+
+#imprimimos un elemmento espe.
+print (numeros [2])
+
+#reasignacion
+numeros[3]= 35
+print (numeros)
+
+#agregamos un nuevo valor al final de 
+numeros.append(60)                
+print (numeros)   
+
+#eliminamos un valor en el arreglo 
+numeros.remove(35)
+print(numeros)
+
+#eliminamos un valor del arreglo usando la pocision
+numeros.pop(4)
+print (numeros)
+
+#
+frutas =["manzana", "fresa", "sandia", "mango", "melon", "platano"]
+frutas.pop(4)
+print(frutas)
+
+frutas.romeve("manzana")
+print(frutas)
